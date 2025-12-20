929b7f80d99ee30cbec3c737b7f49ad1fb628fe5
Julian Garzon <juliangarzon.622@gmail.com>
2025-12-19 19:18:43 -0500

    feat: carga inicial del proyecto Spring Boot

---
+++ src/main/java/com/sportbemy/sportbemy/SportbemyApplication.java
@@ -0,0 +1,13 @@
+package com.sportbemy.sportbemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportbemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportbemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportbemy/sportbemy/SportbemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportbemy.sportbemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportbemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
