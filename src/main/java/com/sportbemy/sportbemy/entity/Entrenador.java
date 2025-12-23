8e1a2f42968a0ba845ffdba94c7e5046faa55527
Julian Garzon <juliangarzon.622@gmail.com>
2025-12-23 17:31:01 -0500

    feat: correccion en los mensajes obligatorios de la entidad Entrenador

--- src/main/java/com/sportbemy/sportbemy/entity/Entrenador.java
+++ src/main/java/com/sportbemy/sportbemy/entity/Entrenador.java
@@ -33,6 +33,6 @@ public class Entrenador {
 
     @Column(length = 100, nullable = false)
     @NotBlank(message = "La especialidad es obligatoria")
-    @Size(min = 3, max = 100, message = "La especialidad debe tener entre 10 y 100 caracteres")
+    @Size(min = 3, max = 100, message = "La especialidad debe tener entre 3 y 100 caracteres")
     private String especialidad;
 }
